ec57be7d453fc548602013e01f7d186be589857b
Ayush Kumar <ayushchaurasiya78@gmail.com>
2024-06-07 12:55:33 +0530

    project setup with next.js, tailwind, typescript, aceternityUI

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <h1 className="text-2xl text-center">Home</h1>
+  );
+}
---
+++ app/table/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <div className='text-white'>Table</div>
+  )
+}
+
+export default Table
